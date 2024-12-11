bc4a305f6c77ec7633c7ec205ac7889a891bdfa4
Tianjiahe-isep <jiti63893@eleve.isep.fr>
2024-12-11 16:42:29 +0100

    Add LectureFichier.java class

--- src/main/java/fr/isep/tian/EcritureFichier.java
+++ src/main/java/fr/isep/tian/EcritureFichier.java
@@ -1,4 +1,22 @@
 package fr.isep.tian;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class EcritureFichier {
+    public static void main(String[] args) {
+    String filePath = "./fichier.txt";
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath,true))) // `true` pour ajouter à la fin
+    {
+        writer.write("Ceci est un exemple de texte.");
+        writer.newLine();
+        writer.write("Une autre ligne de texte.");// Ajoute une nouvelle ligne writer.write("Une autre ligne de texte.");
+    }
+    catch (IOException e) {
+        System.err.println("Erreur lors de l'écriture dans le fichier : " +
+                e.getMessage());
+    }
+}
+
 }
